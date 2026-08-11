4aff372fad3335d93ac93aaaf73578afbd04d481
Giron <hiron@hiron.com>
2026-08-11 20:13:58 +0200

    New stuff

--- services/gateway/src/error.rs
+++ services/gateway/src/error.rs
@@ -14,7 +14,7 @@ pub enum GatewayError {
     Jobs(bvault_jobs::Error),
 }
 
-// Adding an unnecessary change part 8
+// Adding an unnecessary change part 9
 impl From<bvault_meta::Error> for GatewayError {
     fn from(e: bvault_meta::Error) -> Self {
         Self::Meta(e)
